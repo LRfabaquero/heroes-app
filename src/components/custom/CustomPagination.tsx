e779658b15e84251d54f46249053cf812e7b108b
LRfabaquero <franciscobaquerogutierrez@gmail.com>
2026-08-26 14:02:14 -0500

    185. Componente de paginación

--- src/components/custom/CustomPagination.tsx
+++ src/components/custom/CustomPagination.tsx
@@ -33,15 +33,6 @@ export const CustomPagination = ({ totalPages }: Props) => {
                     ))
                 }
 
-
-                {/*<Button variant="outline" size="sm">*/}
-                {/*    2*/}
-                {/*</Button>*/}
-
-                {/*<Button variant="ghost" size="sm" disabled>*/}
-                {/*    <MoreHorizontal className="h-4 w-4" />*/}
-                {/*</Button>*/}
-
                 <Button
                     variant="outline"
                     size="sm"
